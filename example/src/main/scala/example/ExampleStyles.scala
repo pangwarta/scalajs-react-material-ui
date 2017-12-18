304820db5929aa5a57926b77f8ab4c1f09f6914d
Zach Albia <zach.albia@gmail.com>
2017-12-18 16:32:34 +0800

    Cleaned up example CSS a little

--- example/src/main/scala/example/ExampleStyles.scala
+++ example/src/main/scala/example/ExampleStyles.scala
@@ -24,5 +24,8 @@ object ExampleStyles extends StyleSheet.Inline {
     padding(8.px))
 
   val paperShadow2 = style(
-    boxShadow := "0px 1px 5px 0px rgba(255, 0, 0, 0.2), 0px 2px 2px 0px rgba(255, 0, 0, 0.14), 0px 3px 1px -2px rgba(255, 0, 0, 0.12)")
+    boxShadow :=
+      "0px 1px 5px 0px rgba(255, 0, 0, 0.2), " +
+      "0px 2px 2px 0px rgba(255, 0, 0, 0.14), " +
+      "0px 3px 1px -2px rgba(255, 0, 0, 0.12)")
 }

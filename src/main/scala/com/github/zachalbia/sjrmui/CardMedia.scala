6f6b7ba7d647d165464852920d8c4c2d5028f76f
Zach Albia <zach.albia@gmail.com>
2017-12-24 11:08:49 +0800

    Added otherProps to CardMedia

--- src/main/scala/com/github/zachalbia/sjrmui/CardMedia.scala
+++ src/main/scala/com/github/zachalbia/sjrmui/CardMedia.scala
@@ -23,6 +23,23 @@ object CardMedia {
     var src: js.UndefOr[String] = js.native
   }
 
+  private def props(
+      classes:    js.Dictionary[String],
+      className:  js.UndefOr[String],
+      component:  String | ReactElement,
+      image:      js.UndefOr[String],
+      src:        js.UndefOr[String],
+      otherProps: (String, js.Any)*): Props = {
+    val p = js.Dynamic.literal(
+      classes   = classes,
+      className = className,
+      component = component,
+      image     = image,
+      src       = src)
+    addOtherProps(p, otherProps: _*)
+    p.asInstanceOf[Props]
+  }
+
   sealed abstract case class ClassKey(get: String) extends StringType
   object root extends ClassKey("root")
   object rootMedia extends ClassKey("rootMedia")
@@ -34,13 +51,14 @@ object CardMedia {
       className: js.UndefOr[String]    = js.undefined,
       component: String | ReactElement,
       image:     js.UndefOr[String]    = js.undefined,
-      src:       js.UndefOr[String]    = js.undefined)(children: VdomNode*) = {
-    val p = (new js.Object).asInstanceOf[Props]
-    p.classes = classes
-    p.className = className
-    p.component = component
-    p.image = image
-    p.src = src
+      src:       js.UndefOr[String]    = js.undefined)(otherProps: (String, js.Any)*)(children: VdomNode*) = {
+    val p = props(
+      classes,
+      className,
+      component,
+      image,
+      src,
+      otherProps: _*)
     this.component(p)(children: _*)
   }
 }

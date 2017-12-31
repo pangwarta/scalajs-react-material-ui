f892ed42016bc83572a6ca938576f15c1c58d279
Zach Albia <zach.albia@gmail.com>
2017-12-31 12:08:40 +0800

    Added otherProps to CardContent

--- src/main/scala/com/github/zachalbia/sjrmui/CardContent.scala
+++ src/main/scala/com/github/zachalbia/sjrmui/CardContent.scala
@@ -17,13 +17,18 @@ object CardContent {
     var className: js.UndefOr[String] = js.native
   }
 
+  private def props(className: js.UndefOr[String], otherProps: (String, js.Any)*): Props = {
+    val p = js.Dynamic.literal(className = className)
+    addOtherProps(p, otherProps: _*)
+    p.asInstanceOf[Props]
+  }
+
   private val component = JsComponent[Props, Children.Varargs, Null](RawComponent)
 
   def apply(
       className: js.UndefOr[String] = js.undefined
-  )(children: VdomNode*) = {
-    val p = (new js.Object).asInstanceOf[Props]
-    p.className = className
+  )(otherProps: (String, js.Any)*)(children: VdomNode*) = {
+    val p = props(className, otherProps: _*)
     component(p)(children: _*)
   }
 }

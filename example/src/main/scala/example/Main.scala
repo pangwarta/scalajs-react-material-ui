6c797d5998cd3a7606ec8081265c67f97afa3edb
Zach Albia <zach.albia@gmail.com>
2018-01-05 14:16:58 +0800

    Refactored Main, extracted Paper and Card examples

--- example/src/main/scala/example/Main.scala
+++ example/src/main/scala/example/Main.scala
@@ -20,27 +20,8 @@ object Main {
           Grid(container = true)()(
             Grid(item = true, md = 6, sm = 12, xs = 12)()(
               TopSection(),
-              Paper(
-                classes   = Map(
-                  Paper.root -> Seq(ExampleStyles.paper, ExampleStyles.paperExample),
-                  Paper.shadow2 -> ExampleStyles.paperShadow2
-                ),
-                elevation = 5
-              )("foo" -> "bar")(
-                  Typography(component = "p", `type` = Type.body1)()(
-                    "The quick brown fox jumps over the lazy dog."
-                  ),
-                  AlertDialog(),
-                  Checkbox(value = "Check me")(),
-                  Chip(avatar = Avatar()()("ZA"))()
-                ),
-              Card(className = Seq(ExampleStyles.paper, ExampleStyles.card))()(
-                CardHeader(
-                  avatar = Avatar(className = ExampleStyles.cardAvatar.className.value)()("ZA").rawElement,
-                  title  = <.span("Merry Christmas").rawElement
-                )()(),
-                CardActions()()()
-              )
+              PaperExample,
+              CardExample
             )
           )
         ),
@@ -49,4 +30,29 @@ object Main {
     app.renderIntoDOM(dom.document.getElementById("root"))
     ExampleStyles.addToDocument()
   }
+
+  private val PaperExample =
+    Paper(
+      classes   = Map(
+        Paper.root -> Seq(ExampleStyles.paper, ExampleStyles.paperExample),
+        Paper.shadow2 -> ExampleStyles.paperShadow2
+      ),
+      elevation = 5
+    )("foo" -> "bar")(
+        Typography(component = "p", `type` = Type.body1)()(
+          "The quick brown fox jumps over the lazy dog."
+        ),
+        AlertDialog(),
+        Checkbox(value = "Check me")(),
+        Chip(avatar = Avatar()()("ZA"))()
+      )
+
+  private val CardExample =
+    Card(className = Seq(ExampleStyles.paper, ExampleStyles.card))()(
+      CardHeader(
+        avatar = Avatar(className = ExampleStyles.cardAvatar.className.value)()("ZA").rawElement,
+        title  = <.span("Merry Christmas").rawElement
+      )()(),
+      CardActions()()()
+    )
 }

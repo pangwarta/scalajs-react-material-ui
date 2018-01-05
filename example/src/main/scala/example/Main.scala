0a326db2a939cfcf532c2143a01f16cdbd1dad9d
Zach Albia <zach.albia@gmail.com>
2018-01-05 16:52:33 +0800

    Extracted layout to val in Main

--- example/src/main/scala/example/Main.scala
+++ example/src/main/scala/example/Main.scala
@@ -12,33 +12,16 @@ import scalacss.ScalaCssReact._
 object Main {
 
   def main(args: Array[String]): Unit = {
-    val app =
-      <.div(
-        ExampleAppBar(),
-        <.div(
-          ExampleStyles.margin,
-          Grid(container = true)()(
-            Grid(item = true, md = 6, sm = 12, xs = 12)()(
-              TopSection(),
-              PaperExample,
-              CardExample,
-              SimpleExpansionPanel()
-            )
-          )
-        ),
-        SimpleBottomNav()
-      )
-    app.renderIntoDOM(dom.document.getElementById("root"))
+    layout.renderIntoDOM(dom.document.getElementById("root"))
     ExampleStyles.addToDocument()
   }
 
   private val PaperExample =
     Paper(
-      classes   = Map(
+      classes = Map(
         Paper.root -> Seq(ExampleStyles.paper, ExampleStyles.paperExample),
         Paper.shadow2 -> ExampleStyles.paperShadow2
-      ),
-      elevation = 5
+      )
     )("foo" -> "bar")(
         Typography(component = "p", `type` = Type.body1)()(
           "The quick brown fox jumps over the lazy dog."
@@ -55,5 +38,24 @@ object Main {
         title  = <.span("Merry Christmas").rawElement
       )()(),
       CardActions()()()
+    )
+
+  private val layout =
+    <.div(
+      ExampleAppBar(),
+      <.div(
+        ExampleStyles.margin,
+        Grid(container = true)()(
+          Grid(item = true, md = 6, sm = 12, xs = 12)()(
+            TopSection(),
+            PaperExample
+          ),
+          Grid(item = true, md = 6, sm = 12, xs = 12)()(
+            CardExample,
+            SimpleExpansionPanel()
+          )
+        )
+      ),
+      SimpleBottomNav()
     )
 }

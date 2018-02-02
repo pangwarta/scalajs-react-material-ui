6055ab0814ab46a1a0cefc94210b3351e4671ab9
Zach Albia <zach.albia@gmail.com>
2018-02-02 16:34:31 +0800

    Fixed ExpansionPanelSummary js.UndefOr props

--- src/main/scala/com/pangwarta/sjrmui/ExpansionPanelSummary.scala
+++ src/main/scala/com/pangwarta/sjrmui/ExpansionPanelSummary.scala
@@ -22,12 +22,13 @@ object ExpansionPanelSummary {
   }
 
   private def props(
-      centerRipple:             Boolean,
+      centerRipple:             js.UndefOr[Boolean],
       classes:                  js.Dictionary[String],
       className:                js.UndefOr[String],
-      disableRipple:            Boolean,
-      disabled:                 Boolean,
-      focusRipple:              Boolean,
+      component:                js.UndefOr[String | ReactElement],
+      disableRipple:            js.UndefOr[Boolean],
+      disabled:                 js.UndefOr[Boolean],
+      focusRipple:              js.UndefOr[Boolean],
       keyboardFocusedClassName: js.UndefOr[String],
       onBlur:                   OnJSEv1[ReactEvent],
       onClick:                  OnJSEv1[ReactMouseEvent],
@@ -48,32 +49,31 @@ object ExpansionPanelSummary {
       onChange:                 OnJSEv1[ReactEvent],
       otherProps:               (String, js.Any)*
   ): Props = {
-    val p = js.Dynamic.literal(
-      centerRipple             = centerRipple,
-      classes                  = classes,
-      className                = className,
-      disableRipple            = disableRipple,
-      disabled                 = disabled,
-      focusRipple              = focusRipple,
-      keyboardFocusedClassName = keyboardFocusedClassName,
-      onBlur                   = onBlur,
-      onClick                  = onClick,
-      onFocus                  = onFocus,
-      onKeyboardFocus          = onKeyboardFocus,
-      onKeyDown                = onKeyDown,
-      onKeyUp                  = onKeyUp,
-      onMouseDown              = onMouseDown,
-      onMouseLeave             = onMouseLeave,
-      onMouseUp                = onMouseUp,
-      onTouchEnd               = onTouchEnd,
-      onTouchMove              = onTouchMove,
-      onTouchStart             = onTouchStart,
-      role                     = role,
-      tabIndex                 = tabIndex,
-      expanded                 = expanded,
-      expandIcon               = expandIcon,
-      onChange                 = onChange
-    )
+    val p = js.Dynamic.literal(classes = classes)
+    centerRipple.foreach(p.updateDynamic("centerRipple")(_))
+    className.foreach(p.updateDynamic("className")(_))
+    component.foreach(p.updateDynamic("component")(_))
+    disableRipple.foreach(p.updateDynamic("disableRipple")(_))
+    disabled.foreach(p.updateDynamic("disabled")(_))
+    focusRipple.foreach(p.updateDynamic("focusRipple")(_))
+    keyboardFocusedClassName.foreach(p.updateDynamic("keyboardFocusedClassName")(_))
+    onBlur.foreach(p.updateDynamic("onBlur")(_))
+    onClick.foreach(p.updateDynamic("onClick")(_))
+    onFocus.foreach(p.updateDynamic("onFocus")(_))
+    onKeyboardFocus.foreach(p.updateDynamic("onKeyboardFocus")(_))
+    onKeyDown.foreach(p.updateDynamic("onKeyDown")(_))
+    onKeyUp.foreach(p.updateDynamic("onKeyUp")(_))
+    onMouseDown.foreach(p.updateDynamic("onMouseDown")(_))
+    onMouseLeave.foreach(p.updateDynamic("onMouseLeave")(_))
+    onMouseUp.foreach(p.updateDynamic("onMouseUp")(_))
+    onTouchEnd.foreach(p.updateDynamic("onTouchEnd")(_))
+    onTouchMove.foreach(p.updateDynamic("onTouchMove")(_))
+    onTouchStart.foreach(p.updateDynamic("onTouchStart")(_))
+    role.foreach(p.updateDynamic("role")(_))
+    tabIndex.foreach(p.updateDynamic("tabIndex")(_))
+    expanded.foreach(p.updateDynamic("expanded")(_))
+    expandIcon.foreach(p.updateDynamic("expandIcon")(_))
+    onChange.foreach(p.updateDynamic("onChange")(_))
     addOtherProps(p, otherProps: _*)
     p.asInstanceOf[Props]
   }
@@ -91,12 +91,13 @@ object ExpansionPanelSummary {
   private def component = JsComponent[Props, Children.Varargs, Null](RawComponent)
 
   def apply(
-      centerRipple:             Boolean                           = false,
+      centerRipple:             js.UndefOr[Boolean]               = js.undefined,
       classes:                  Map[ClassKey, String]             = Map.empty,
       className:                js.UndefOr[String]                = js.undefined,
-      disableRipple:            Boolean                           = false,
-      disabled:                 Boolean                           = false,
-      focusRipple:              Boolean                           = false,
+      component:                js.UndefOr[String | ReactElement] = js.undefined,
+      disableRipple:            js.UndefOr[Boolean]               = js.undefined,
+      disabled:                 js.UndefOr[Boolean]               = js.undefined,
+      focusRipple:              js.UndefOr[Boolean]               = js.undefined,
       keyboardFocusedClassName: js.UndefOr[String]                = js.undefined,
       onBlur:                   ReactHandler1[ReactEvent]         = js.undefined,
       onClick:                  ReactHandler1[ReactMouseEvent]    = js.undefined,
@@ -120,6 +121,7 @@ object ExpansionPanelSummary {
       centerRipple,
       classes,
       className,
+      component,
       disableRipple,
       disabled,
       focusRipple,

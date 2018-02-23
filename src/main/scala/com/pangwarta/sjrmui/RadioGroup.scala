26ba9178cb9892efc3918957329ba8646d6028b3
Zach Albia <zach.albia@gmail.com>
2018-02-23 18:51:50 +0800

    Added RadioGroup component wrapper

---
+++ src/main/scala/com/pangwarta/sjrmui/RadioGroup.scala
@@ -0,0 +1,61 @@
+package com.pangwarta.sjrmui
+
+import japgolly.scalajs.react._
+import japgolly.scalajs.react.vdom.VdomNode
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+object RadioGroup {
+
+  @JSImport("material-ui", "RadioGroup")
+  @js.native
+  private object RawComponent extends js.Object
+
+  @js.native
+  private[sjrmui] trait Props extends js.Object {
+    var name: js.UndefOr[String] = js.native
+    var onBlur: OnJSEv1[ReactEvent] = js.native
+    var onChange: OnJSEv2[ReactEvent, String] = js.native
+    var onKeyDown: OnJSEv1[ReactKeyboardEventFromHtml] = js.native
+    var value: js.UndefOr[String] = js.native
+  }
+
+  private def props(
+      name:       js.UndefOr[String],
+      onBlur:     OnJSEv1[ReactEvent],
+      onChange:   OnJSEv2[ReactEvent, String],
+      onKeyDown:  OnJSEv1[ReactKeyboardEventFromHtml],
+      value:      js.UndefOr[String],
+      otherProps: (String, js.Any)*
+  ): Props = {
+    val p = js.Dynamic.literal()
+    name.foreach(p.updateDynamic("name")(_))
+    onBlur.foreach(p.updateDynamic("onBlur")(_))
+    onChange.foreach(p.updateDynamic("onChange")(_))
+    onKeyDown.foreach(p.updateDynamic("onKeyDown")(_))
+    value.foreach(p.updateDynamic("value")(_))
+    addOtherProps(p, otherProps: _*)
+    p.asInstanceOf[Props]
+  }
+
+  private val component = JsComponent[Props, Children.Varargs, Null](RawComponent)
+
+  def apply(
+      name:      js.UndefOr[String]                        = js.undefined,
+      onBlur:    ReactHandler1[ReactEvent]                 = js.undefined,
+      onChange:  ReactHandler2[ReactEvent, String]         = js.undefined,
+      onKeyDown: ReactHandler1[ReactKeyboardEventFromHtml] = js.undefined,
+      value:     js.UndefOr[String]                        = js.undefined
+  )(otherProps: (String, js.Any)*)(children: VdomNode*) = {
+    val p = props(
+      name,
+      onBlur,
+      onChange,
+      onKeyDown,
+      value,
+      otherProps: _*
+    )
+    this.component(p)(children: _*)
+  }
+}

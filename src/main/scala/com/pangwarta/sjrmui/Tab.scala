04eadc05e630c0b48a85c01ee6d66a63439f2c68
Zach Albia <zach.albia@gmail.com>
2018-03-05 17:35:10 +0800

    Checked Tab component wrapper

--- src/main/scala/com/pangwarta/sjrmui/Tab.scala
+++ src/main/scala/com/pangwarta/sjrmui/Tab.scala
@@ -4,7 +4,6 @@ import japgolly.scalajs.react._
 import japgolly.scalajs.react.raw._
 import japgolly.scalajs.react.vdom.VdomNode
 
-
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 import scala.scalajs.js.|
@@ -28,30 +27,30 @@ object Tab {
     var className: js.UndefOr[String] = js.native
     var disabled: js.UndefOr[Boolean] = js.native
     var fullWidth: js.UndefOr[Boolean] = js.native
-    var icon: js.UndefOr[String|ReactElement] = js.native
-    var indicator: js.UndefOr[String|ReactElement] = js.native
-    var label: js.UndefOr[String|ReactElement] = js.native
+    var icon: js.UndefOr[String | ReactElement] = js.native
+    var indicator: js.UndefOr[String | ReactElement] = js.native
+    var label: js.UndefOr[String | ReactElement] = js.native
     var onChange: OnJSEv1[ReactEvent] = js.native
     var onClick: OnJSEv1[ReactEvent] = js.native
     var selected: js.UndefOr[Boolean] = js.native
     var style: js.UndefOr[js.Object] = js.native
     var value: js.UndefOr[A] = js.native
   }
-  private def props[A] (
-                         classes: js.Dictionary[String],
-                         className: js.UndefOr[String],
-                         disabled: js.UndefOr[Boolean],
-                         fullWidth: js.UndefOr[Boolean],
-                         icon: js.UndefOr[String|ReactElement],
-                         indicator: js.UndefOr[String|ReactElement],
-                         label: js.UndefOr[String|ReactElement],
-                         onChange: OnJSEv1[ReactEvent],
-                         onClick: OnJSEv1[ReactEvent],
-                         selected: js.UndefOr[Boolean],
-                         style: js.UndefOr[js.Object],
-                         value: js.UndefOr[A],
-                         otherProps: (String, js.Any)*
-                       ): Props[A] = {
+  private def props[A](
+      classes:    js.Dictionary[String],
+      className:  js.UndefOr[String],
+      disabled:   js.UndefOr[Boolean],
+      fullWidth:  js.UndefOr[Boolean],
+      icon:       js.UndefOr[String | ReactElement],
+      indicator:  js.UndefOr[String | ReactElement],
+      label:      js.UndefOr[String | ReactElement],
+      onChange:   OnJSEv1[ReactEvent],
+      onClick:    OnJSEv1[ReactEvent],
+      selected:   js.UndefOr[Boolean],
+      style:      js.UndefOr[js.Object],
+      value:      js.UndefOr[A],
+      otherProps: (String, js.Any)*
+  ): Props[A] = {
     val p = js.Dynamic.literal(classes = classes)
     className.foreach(v => p.updateDynamic("className")(v))
     disabled.foreach(v => p.updateDynamic("disabled")(v))
@@ -70,40 +69,39 @@ object Tab {
   }
 
   sealed abstract case class ClassKey(get: String) extends StringType
-  object root extends ClassKey( "root")
-  object rootLabelIcon extends ClassKey( "rootLabelIcon")
-  object rootInherit extends ClassKey( "rootInherit")
-  object rootPrimary extends ClassKey( "rootPrimary")
-  object rootPrimarySelected extends ClassKey( "rootPrimarySelected")
-  object rootPrimaryDisabled extends ClassKey( "rootPrimaryDisabled")
-  object rootSecondary extends ClassKey( "rootSecondary")
-  object rootSecondarySelected extends ClassKey( "rootSecondarySelected")
-  object rootSecondaryDisabled extends ClassKey( "rootSecondaryDisabled")
-  object rootInheritSelected extends ClassKey( "rootInheritSelected")
-  object rootInheritDisabled extends ClassKey( "rootInheritDisabled")
-  object fullWidth extends ClassKey( "fullWidth")
-  object wrapper extends ClassKey( "wrapper")
-  object labelContainer extends ClassKey( "labelContainer")
-  object label extends ClassKey( "label")
-  object labelWrapped extends ClassKey( "labelWrapped")
-
+  object root extends ClassKey("root")
+  object rootLabelIcon extends ClassKey("rootLabelIcon")
+  object rootInherit extends ClassKey("rootInherit")
+  object rootPrimary extends ClassKey("rootPrimary")
+  object rootPrimarySelected extends ClassKey("rootPrimarySelected")
+  object rootPrimaryDisabled extends ClassKey("rootPrimaryDisabled")
+  object rootSecondary extends ClassKey("rootSecondary")
+  object rootSecondarySelected extends ClassKey("rootSecondarySelected")
+  object rootSecondaryDisabled extends ClassKey("rootSecondaryDisabled")
+  object rootInheritSelected extends ClassKey("rootInheritSelected")
+  object rootInheritDisabled extends ClassKey("rootInheritDisabled")
+  object fullWidth extends ClassKey("fullWidth")
+  object wrapper extends ClassKey("wrapper")
+  object labelContainer extends ClassKey("labelContainer")
+  object label extends ClassKey("label")
+  object labelWrapped extends ClassKey("labelWrapped")
 
   private def component[A] = JsComponent[Props[A], Children.Varargs, Null](RawComponent)
 
   def apply[A](
-                classes:   Map[ClassKey, String] = Map.empty,
-                className: js.UndefOr[String] = js.undefined,
-                disabled: js.UndefOr[Boolean] = js.undefined,
-                fullWidth: js.UndefOr[Boolean] = js.undefined,
-                icon: js.UndefOr[String|ReactElement] = js.undefined,
-                indicator: js.UndefOr[String|ReactElement] = js.undefined,
-                label: js.UndefOr[String|ReactElement] = js.undefined,
-                onChange: OnJSEv1[ReactEvent] = js.undefined,
-                onClick: OnJSEv1[ReactEvent] = js.undefined,
-                selected: js.UndefOr[Boolean] = js.undefined,
-                style: js.UndefOr[js.Object] = js.undefined,
-                value: js.UndefOr[A] = js.undefined,
-              )(otherProps: (String, js.Any)*)(children: VdomNode*) = {
+      classes:   Map[ClassKey, String]             = Map.empty,
+      className: js.UndefOr[String]                = js.undefined,
+      disabled:  js.UndefOr[Boolean]               = js.undefined,
+      fullWidth: js.UndefOr[Boolean]               = js.undefined,
+      icon:      js.UndefOr[String | ReactElement] = js.undefined,
+      indicator: js.UndefOr[String | ReactElement] = js.undefined,
+      label:     js.UndefOr[String | ReactElement] = js.undefined,
+      onChange:  OnJSEv1[ReactEvent]               = js.undefined,
+      onClick:   OnJSEv1[ReactEvent]               = js.undefined,
+      selected:  js.UndefOr[Boolean]               = js.undefined,
+      style:     js.UndefOr[js.Object]             = js.undefined,
+      value:     js.UndefOr[A]                     = js.undefined
+  )(otherProps: (String, js.Any)*)(children: VdomNode*) = {
     val p = props(
       classes,
       className,
